a43cb8c377c97edd3dacacfd631df2f6317b5b81
Tim Chumbash <posseslash@gmail.com>
2025-12-06 13:12:04 +0000

    bonus done

---
+++ get_next_line_bonus.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/06 13:08:08 by tchumbas          #+#    #+#             */
+/*   Updated: 2025/12/06 13:08:11 by tchumbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_read_to_nl_vars
+{
+	int		i;
+	int		buf_len_nl;
+	ssize_t	bytesread;
+}			t_read_to_nl_vars;
+
+void		*ft_calloc(size_t nmemb, size_t size);
+
+char		*append_line(char *s1, char const *buf);
+char		*ft_strchr(const char *s, int c);
+char		*get_next_line(int fd);
+char		*read_until_nl(int fd, char *buf, char *line);
+
+size_t		ft_strlen_nl(const char *s);
+
+#endif
