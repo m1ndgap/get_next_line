54e42a4e715eff0d4f8745ad6e7c8e451572adce
Tim Chumbash <posseslash@gmail.com>
2025-12-13 13:38:36 +0000

    all tests

---
+++ TESTS/main_big_line_NL.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_big_line_NL.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 15:45:38 by tchumbas          #+#    #+#             */
+/*   Updated: 2025/12/13 13:30:24 by tchumbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include "get_next_line.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*file;
+	int		fd;
+	char	*line;
+
+	file = "TESTS/big_line_with_nl";
+	fd = open(file, O_RDONLY);
+	// file offset (mark of current position @ file) set to beginning of file.
+	line = NULL;
+	if (fd == -1)
+		printf("fd = %d => cannot open '%s'\n", fd, file);
+	else
+	{
+		printf("========================================================\n");
+		printf("fd = %d => '%s' opened successfully.\n", fd, file);
+		printf("========================================================\n");
+		}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("\n***** Next line: *****\n");
+		printf("%s\n", line);
+		free(line);
+	}
+	close(fd);
+	printf("\n###########################\n");
+	printf("####### END OF TEST #######\n");
+	printf("###########################\n\n\n");
+	return (0);
+}
